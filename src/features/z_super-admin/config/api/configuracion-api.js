41f18085df07db3278e9a4d4a4f087c090342f37
Joel Rodriguez <joldeluxe@gmail.com>
2026-07-28 09:54:49 -0600

    fix(config): handle autonomous settings response correctly

--- src/features/z_super-admin/config/api/configuracion-api.js
+++ src/features/z_super-admin/config/api/configuracion-api.js
@@ -5,11 +5,11 @@ import api from '@/lib/axios';
  * @param {AbortSignal} [signal] - Señal de cancelación opcional.
  */
 export const getAutonomosConfig = (signal) =>
-  api.get('/api/configuracion/autonomos', { signal }).then((res) => res.data);
+  api.get('/api/configuracion/autonomos', { signal });
 
 /**
  * Actualizar estado del feature flag de mantenimientos autónomos.
  * @param {boolean} habilitado
  */
 export const patchAutonomosConfig = (habilitado) =>
-  api.patch('/api/configuracion/autonomos', { habilitado }).then((res) => res.data);
+  api.patch('/api/configuracion/autonomos', { habilitado });
